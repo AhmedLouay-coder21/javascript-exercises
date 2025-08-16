222cb7eb476ce878bc9a53e82c5230a4b37b258d
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2025-08-16 21:43:35 +0300

    Solve: Commit Exercises Solution from 1 to 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = [];
+    let numberOfElements = array.length;
+    for(let i = 0; i < numberOfElements; i++)
+    {
+        if (!remove.includes(array[i]))
+        {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
